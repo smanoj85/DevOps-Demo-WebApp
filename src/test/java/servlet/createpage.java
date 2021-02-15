eb3902ccdeac1062e98294abd16340e34f646bc6
ManojKumar Selvaraju <smanoj85@gmail.com>
2021-02-15 01:44:32 -0500

    Updated the IP Address to work correctly

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://35.209.44.129:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://35.208.44.46:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
